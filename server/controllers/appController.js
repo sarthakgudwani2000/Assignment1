afc22fb6a1fa3f84271665ee39d792dd02f5e4bf
Sarthak Gudwani <gudwanisarthak2000@gmail.com>
2024-02-05 13:39:01 +0530

    Created generateOTP, verifyOTP, createResetSession and resetPassword Routes

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -2,16 +2,17 @@ import UserModel from '../model/User.model.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import ENV from '../config.js'
+import otpGenerator from 'otp-generator';
 
-export async function verifyUser(req, res, next){
-    try {        
+export async function verifyUser(req, res, next) {
+    try {
         const { username } = req.method == "GET" ? req.query : req.body;
 
         let exist = await UserModel.findOne({ username });
-        if(!exist) return res.status(404).send({ error : "Can't find User!"});
+        if (!exist) return res.status(404).send({ error: "Can't find User!" });
         next();
     } catch (error) {
-        return res.status(404).send({ error: "Authentication Error"});
+        return res.status(404).send({ error: "Authentication Error" });
     }
 }
 
@@ -39,7 +40,7 @@ export async function register(req, res) {
 
         Promise.all([existUsername, existEmail])
             .then(() => {
-                if  (password) {
+                if (password) {
                     bcrypt.hash(password, 10)
                         .then(hashedPassword => {
 
@@ -79,66 +80,66 @@ export async function login(req, res) {
                 bcrypt.compare(password, user.password)
                     .then(passwordCheck => {
 
-                        if(!passwordCheck) return res.status(400).send({ error: "Don't have Password"});
+                        if (!passwordCheck) return res.status(400).send({ error: "Don't have Password" });
 
                         const token = jwt.sign({
-                                        userId: user._id,
-                                        username : user.username
-                                    }, ENV.JWT_SECRET , { expiresIn : "24h"});
+                            userId: user._id,
+                            username: user.username
+                        }, ENV.JWT_SECRET, { expiresIn: "24h" });
 
                         return res.status(200).send({
                             msg: "Login Successful...!",
                             username: user.username,
                             token
-                        });                                    
+                        });
 
                     })
-                    .catch(error =>{
-                        return res.status(400).send({ error: "Password does not Match"})
+                    .catch(error => {
+                        return res.status(400).send({ error: "Password does not Match" })
                     })
             })
-            .catch( error => {
-                return res.status(404).send({ error : "Username not Found"});
+            .catch(error => {
+                return res.status(404).send({ error: "Username not Found" });
             })
 
     } catch (error) {
-        return res.status(500).send({ error});
+        return res.status(500).send({ error });
     }
 }
 
-export async function getUser(req,res){
+export async function getUser(req, res) {
     const { username } = req.params;
 
     try {
-        if(!username) return res.status(501).send({ error: "Invalid Username"});
+        if (!username) return res.status(501).send({ error: "Invalid Username" });
 
-        UserModel.findOne({ username }, function(err, user) {
-            if(err) return res.status(500).send({ err });
-            if(!user) return res.status(501).send({ error : "Couldn't Find the User"});
+        UserModel.findOne({ username }, function (err, user) {
+            if (err) return res.status(500).send({ err });
+            if (!user) return res.status(501).send({ error: "Couldn't Find the User" });
 
             const { password, ...rest } = Object.assign({}, user.toJSON());
 
             return res.status(201).send(rest);
         })
     } catch (error) {
-        return res.status(404).send({ error : "Cannot Find User Data"});
+        return res.status(404).send({ error: "Cannot Find User Data" });
     }
 }
 
-export async function updateUser(req,res){
+export async function updateUser(req, res) {
     try {
-        const id = req.query.id;
-        
-        if(id) {
+        const { userId } = req.user;
+
+        if (userId) {
             const body = req.body;
 
-            UserModel.updateOne({ _id : id}, body, function(err, data){
-                if(err) throw err;
+            UserModel.updateOne({ _id: userId }, body, function (err, data) {
+                if (err) throw err;
 
-                return res.status(200).send({msg : "Record updated successfully"})
+                return res.status(200).send({ msg: "Record updated successfully" })
             })
         } else {
-            return res.status(401).send({ error : "User Not Found"});
+            return res.status(401).send({ error: "User Not Found" });
         }
     } catch (error) {
         return res.status(404).send({ error });
@@ -146,17 +147,63 @@ export async function updateUser(req,res){
 }
 
 export async function generateOTP(req, res) {
-    res.json('generateOTP route');
+    req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false })
+
+    res.status(201).send({ code: req.app.locals.OTP })
 }
 
 export async function verifyOTP(req, res) {
-    res.json('verifyOTP route');
+    const { code } = req.query;
+
+    if (parseInt(req.app.locals.OTP) === parseInt(code)) {
+        req.app.locals.OTP = null;
+        req.app.locals.resetSession = true;
+
+        return res.status(201).send({ msg: "OTP Verified!" })
+    }
+    return res.status(400).send({ error: "Invalid OTP" });
 }
 
 export async function createResetSession(req, res) {
-    res.json('createResetSession route');
+    if (req.app.locals.resetSession) {
+        req.app.locals.resetSession = false;
+        return res.status(201).send({ msg: "Reset Session Created!" })
+    }
+    return res.status(440).send({ error: "Reset Session Already Created!" });
 }
 
 export async function resetPassword(req, res) {
-    res.json('resetPassword route');
+    try {
+
+        if (!req.app.locals.resetSession) {
+            return res.status(440).send({ error: "Reset Session Already Created!" });
+        }
+        const { username, password } = req.body;
+
+        try {
+            UserModel.findOne({ username })
+                .then(user => {
+                    bcrypt.hash(password, 10)
+                        .then(hashedPassword => {
+                            UserModel.updateOne({ username: user.username },
+                                { password: hashedPassword }, function (err, data) {
+                                    if (err) throw err;
+                                    return res.status(200).send({ msg: "Record updated" })
+                                })
+                        })
+                        .catch(e => {
+                            return res.status(500).send({
+                                error: "Enable to hashed password"
+                            })
+                        })
+                })
+                .catch(error => {
+                    return res.status(404).send({ error: "Username not found" });
+                })
+        } catch (error) {
+            return res.status(500).send({ error });
+        }
+    } catch (error) {
+        return res.status(401).send({ error });
+    }
 }
--- server/router/route.js
+++ server/router/route.js
@@ -2,18 +2,19 @@ import { Router } from "express";
 const router = Router();
 
 import * as controller from '../controllers/appController.js'
+import Auth, { localVariables } from "../middleware/auth.js";
 
 router.route('/register').post(controller.register); 
-// router.route('/registerMail').post();
+router.route('/registerMail').post();
 router.route('/authenticate').post((req, res) => res.end()); 
 router.route('/login').post(controller.verifyUser, controller.login); 
 
 router.route('/user/:username').get(controller.getUser) 
-router.route('/generateOTP').get(controller.generateOTP) 
+router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP) 
 router.route('/verifyOTP').get(controller.verifyOTP) 
 router.route('/createResetSession').get(controller.createResetSession) 
 
-router.route('/updateuser').put(controller.updateUser); 
-router.route('/resetPassword').put(controller.resetPassword); 
+router.route('/updateuser').put(Auth, controller.updateUser); 
+router.route('/resetPassword').put(controller.verifyUser, controller.resetPassword); 
 
 export default router;
